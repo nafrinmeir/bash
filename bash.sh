297c4e7da02bbbbe6bae8165a802b48886d07474
nafrinmeir <45077386+nafrinmeir@users.noreply.github.com>
2023-07-20 15:36:04 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,8 +5,8 @@ jenkins_container="d1640cf7125b"
 nginx_container="14e8ecd0fc82"
 
 # Define the source and destination paths within the containers
-source_path="/path/to/source/file.txt"
-destination_path="/path/to/destination/file.txt"
+source_path="/var/jenkins_home/workspace/ptwo/*.*"
+destination_path="/usr/share/nginx/html/"
 
 # Use docker cp to copy the file from Jenkins to Nginx container
 docker cp "$jenkins_container:$source_path" "$destination_path"
